90b759aed96d1abe00358f1f6143152bb951dafc
Sarah Lettmann <mail@sarah-lettmann.dev>
2020-11-28 14:43:30 +0100

    initial vue setup and components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <real-digital-form action="/script.php" method="POST">
+        <real-digital-textfield name="name" validation="[a-z]+"></real-digital-textfield>
+        <real-digital-textfield name="phone" validation="[0-9]+"></real-digital-textfield>
+        <real-digital-textfield name="subject"></real-digital-textfield>
+        <real-digital-button>
+            Send
+        </real-digital-button>
+    </real-digital-form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RealDigitalForm from './components/RealDigitalForm.vue'
+import RealDigitalTextfield from './components/RealDigitalTextfield.vue'
+import RealDigitalButton from './components/RealDigitalButton.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'real-digital-form': RealDigitalForm,
+    'real-digital-textfield': RealDigitalTextfield,
+    'real-digital-button': RealDigitalButton, 
   }
 }
 </script>
